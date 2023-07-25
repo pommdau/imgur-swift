953fdef7f381a909705346faee679f3e6909ae40
IKEH1024 <pondau1024@gmail.com>
2023-07-25 17:53:42 +0900

    feat: add sample project

---
+++ ImgurSwiftExample/ImgurSwiftExample/ImgurSwiftExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImgurSwiftExampleApp.swift
+//  ImgurSwiftExample
+//
+//  Created by HIROKI IKEUCHI on 2023/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ImgurSwiftExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
