fbb32bedd988bb9cb2837b4b892869018c668356
IKEH1024 <pondau1024@gmail.com>
2023-07-26 10:05:41 +0900

    wip

---
+++ ImgurSwiftExample/ImgurSwiftExample/Core/data/api/APIConstants.swift
@@ -0,0 +1,13 @@
+//
+//  APIConstants.swift
+//  ImgurSwiftExample
+//
+//  Created by HIROKI IKEUCHI on 2023/07/25.
+//
+
+import Foundation
+
+enum APIConstants {
+    static let clientID = ""
+    static let clientSecret = ""
+}
